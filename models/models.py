2a51d0c6ec8ff8371871003070f2715d034f4374
santiagomd11 <santiagoe4333@gmail.com>
2023-10-18 15:39:44 -0500

    Adding inital structure to the repository

---
+++ models/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(128))
+    password = db.Column(db.String(150))
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        include_relationships = True
+        load_instance = True
+
+    id = fields.String()
