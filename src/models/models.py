17b5ce5c7a2580d1ce0a14a3316ecc0fbc5288a3
Steve <sj.arevalo@uniandes.edu.co>
2023-10-22 15:35:06 +0200

    Added new model relationship

--- src/models/models.py
+++ src/models/models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     tasks = db.relationship('Task', backref='user', lazy=True)
     
     
-class FileExtensions(Enum):
+class FileExtensions(str, Enum):
     MP4 = 'mp4'
     WEBM = 'webm'
     AVI = 'avi'
@@ -30,12 +30,14 @@ class Task(db.Model):
     original_file_url = db.Column(db.String(2000))
     converted_file_url = db.Column(db.String(2000))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    conversion_file = db.relationship('ConversionFile', uselist=False, backref='task')
     
 class ConversionFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(20))
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), unique=True)
         
     
 
@@ -52,5 +54,17 @@ class TaskSchema(SQLAlchemyAutoSchema):
         model = Task
         include_relationships = True
         load_instance = True
-    
+        
+        
     id = fields.String()
+    
+    original_file_extension = fields.String(dump_only=True)
+    
+    def dump(self, obj, *, many=None, **kwargs):
+        if not isinstance(obj, list):
+            obj.original_file_extension = obj.original_file_extension.value
+            obj.converted_file_extension = obj.converted_file_extension.value
+        return super().dump(obj, many=many, **kwargs)
+
+
+    
