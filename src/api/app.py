da5c207e7e5ded492d6f0013328014e2ff0ad067
santiagomd11 <santiagoe4333@gmail.com>
2023-11-12 05:01:30 -0500

    Adding endpoint for health check on gcp

--- src/api/app.py
+++ src/api/app.py
@@ -24,6 +24,11 @@ db.create_all()
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 api = Api(app)
+
+@app.route('/health')
+def health_check():
+    return 'OK', 200
+
 api.add_resource(ViewRegister, "/user-register")
 api.add_resource(ViewLogin, "/login")
 api.add_resource(ViewTask, "/task/<int:id_task>")
